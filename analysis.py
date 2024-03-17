c3e78f8a0ae89a759ed5732add54b8c09b2d28b4
Aurelien Giuglaris-Michael <aurelien.giuglarism@gmail.com>
2024-03-17 19:53:13 +0100

    started correlation analysis and implemented KPSS test
    tested for stationarity using KPSS test + will later have to do it with correlogram
    and then check if I need to adjust seasonality or not

--- analysis.py
+++ analysis.py
@@ -2,3 +2,44 @@ import csv
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import preprocessing 
+from statsmodels.tsa.stattools import kpss
+
+# TODO : Evaluate the stationarity of the data using correlogram
+
+##### STATISTICAL TESTS #####
+
+# KPSS Test for Stationarity
+# Null Hypothesis: the data is stationary around a constant mean, exhibiting a linear trend component
+# The Null Hypothesis is rejected if the p-value is less than the significance level
+# The Null Hypotheis is rejected if the test statistic is greater than the critical value (5% significance level)
+def is_stationary_with_kpss(data, significance_level=0.05):
+    test = kpss(data, regression='ct') # 'ct' for constant and trend component more appropriate for financial time series than 'c' for constant only
+    test_statistic = test[0]
+    p_value = test[1]
+    critical_value_ref = test[3]
+    print("KPSS p-value: {:0.5f}".format(p_value))
+    print("KPSS test statistic: {:0.5f}".format(test_statistic))
+    print("KPSS critical value: ", critical_value_ref)
+    return p_value > significance_level
+
+##### MAIN #####
+
+## Load the data from pre-processing ##
+wine_df, watch_df, art_df, crypto_df, gold_df, sp500_df, cpi_df, bond_yield_df = preprocessing.main()
+
+## Check for stationarity using Statistical Test and Correlogram ##
+# Statistical Test : All data points are Non-Stationary
+
+# Asset Data
+print("TEST FOR STATIONARITY:", is_stationary_with_kpss(wine_df['Index Value'])) # Not stationary at 5% significance level
+print("TEST FOR STATIONARITY:", is_stationary_with_kpss(watch_df['Index Value'])) # Not stationary at 5% significance level
+print("TEST FOR STATIONARITY:", is_stationary_with_kpss(art_df['Index Value'])) # Not stationary at 5% significance level
+
+# Correlated Variables
+print("TEST FOR STATIONARITY:", is_stationary_with_kpss(crypto_df['Index Value'])) # Not stationary at 5% significance level
+print("TEST FOR STATIONARITY:", is_stationary_with_kpss(gold_df['Index Value'])) # Not stationary at 5% significance level
+print("TEST FOR STATIONARITY:", is_stationary_with_kpss(sp500_df['Real'])) # Not stationary at 5% significance level
+print("TEST FOR STATIONARITY:", is_stationary_with_kpss(cpi_df['CPI_Index'])) # Not stationary at 5% significance level
+
+
--- preprocessing.py
+++ preprocessing.py
@@ -384,158 +384,161 @@ def log_transform_cpi(cpi_df):
 
 ##### MAIN #####
 
+def main():
+
 ## Retrieve all data and convert them to monthly time frames under the same currency (USD) ##
+    
+    # Get GBP to EUR rates
+    GBP_rates = get_EURGBP_rates_yearly('data\GBP_EUR_Historical_Rates.csv')
+    # plt.plot(GBP_rates['Year'], GBP_rates['Rate'])
+    # plt.xlabel('Year')
+    # plt.ylabel('Rate')
+    # plt.title('EUR to GBP (Yearly Average)')
+    # plt.show()
 
-# Get GBP to EUR rates
-GBP_rates = get_EURGBP_rates_yearly('data\GBP_EUR_Historical_Rates.csv')
-# plt.plot(GBP_rates['Year'], GBP_rates['Rate'])
-# plt.xlabel('Year')
-# plt.ylabel('Rate')
-# plt.title('EUR to GBP (Yearly Average)')
-# plt.show()
+    # Get EUR to USD rates
+    USD_rates = get_EURUSD_rates_yearly('data\EUR_USD_Historical_Rates.csv')
+    # plt.plot(USD_rates['Year'], USD_rates['Rate'])
+    # plt.xlabel('Year')
+    # plt.ylabel('Rate')
+    # plt.title('EUR to USD (Yearly Average)')
+    # plt.show()
 
-# Get EUR to USD rates
-USD_rates = get_EURUSD_rates_yearly('data\EUR_USD_Historical_Rates.csv')
-# plt.plot(USD_rates['Year'], USD_rates['Rate'])
-# plt.xlabel('Year')
-# plt.ylabel('Rate')
-# plt.title('EUR to USD (Yearly Average)')
-# plt.show()
+    # Get GBP to USD rates
+    USD2_rates = get_GBPUSD_rates_yearly('data\GBP_USD_Historical_Rates.csv')
+    # plt.plot(USD2_rates['Year'], USD2_rates['Rate'])
+    # plt.xlabel('Year')
+    # plt.ylabel('Rate')
+    # plt.title('GBP to USD (Yearly Average)')
+    # plt.show()
 
-# Get GBP to USD rates
-USD2_rates = get_GBPUSD_rates_yearly('data\GBP_USD_Historical_Rates.csv')
-# plt.plot(USD2_rates['Year'], USD2_rates['Rate'])
-# plt.xlabel('Year')
-# plt.ylabel('Rate')
-# plt.title('GBP to USD (Yearly Average)')
-# plt.show()
+    # Watch EUR
+    watch_df_EUR = get_watch_data('data\Watches\Watch_Index.csv')
+    # plt.plot(watch_df_EUR['Date'], watch_df_EUR['Index Value'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Index Value (Monthly Average)')
+    # plt.title('(Custom Weighted) Watch Index Monthly Average EUR')
+    # plt.show()
 
-# Watch EUR
-watch_df_EUR = get_watch_data('data\Watches\Watch_Index.csv')
-# plt.plot(watch_df_EUR['Date'], watch_df_EUR['Index Value'])
-# plt.xlabel('Date')
-# plt.ylabel('Index Value (Monthly Average)')
-# plt.title('(Custom Weighted) Watch Index Monthly Average EUR')
-# plt.show()
+    # EUR was created in 1999, so we need to convert to USD (Not inflation adjusted)
+    watch_df = convert_to_USD(watch_df_EUR, USD_rates)
+    # plt.plot(watch_df['Date'], watch_df['Index Value'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Index Value (Monthly Average)')
+    # plt.title('(Custom Weighted) Watch Index Monthly Average USD')
+    # plt.show()
 
-# EUR was created in 1999, so we need to convert to USD (Not inflation adjusted)
-watch_df = convert_to_USD(watch_df_EUR, USD_rates)
-# plt.plot(watch_df['Date'], watch_df['Index Value'])
-# plt.xlabel('Date')
-# plt.ylabel('Index Value (Monthly Average)')
-# plt.title('(Custom Weighted) Watch Index Monthly Average USD')
-# plt.show()
+    # Wine GBP
+    wine_df_GBP = get_wine_data('data\Wine\Liv-Ex 100 Index.csv')
+    # plt.plot(wine_df_GBP['Date'], wine_df_GBP['Index Value'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Index Value')
+    # plt.title('Liv-Ex 100 Index (Monthly Average)')
+    # plt.show()
 
-# Wine GBP
-wine_df_GBP = get_wine_data('data\Wine\Liv-Ex 100 Index.csv')
-# plt.plot(wine_df_GBP['Date'], wine_df_GBP['Index Value'])
-# plt.xlabel('Date')
-# plt.ylabel('Index Value')
-# plt.title('Liv-Ex 100 Index (Monthly Average)')
-# plt.show()
+    # Wine needs to be converted to USD (Not inflation adjusted)
+    wine_df = convert_to_USD(wine_df_GBP, USD2_rates)
+    # plt.plot(wine_df['Date'], wine_df['Index Value'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Index Value')
+    # plt.title('Liv-Ex 100 Index (Monthly Average) USD')
+    # plt.show()
 
-# Wine needs to be converted to USD (Not inflation adjusted)
-wine_df = convert_to_USD(wine_df_GBP, USD2_rates)
-# plt.plot(wine_df['Date'], wine_df['Index Value'])
-# plt.xlabel('Date')
-# plt.ylabel('Index Value')
-# plt.title('Liv-Ex 100 Index (Monthly Average) USD')
-# plt.show()
+    # Art GBP (already inflation adjusted)
+    art_df_GBP = get_art_data('data\Art\All Art Index Family\index_values.json')
+    # plt.plot(art_df_GBP['Date'], art_df_GBP['Index Value'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Index Value')
+    # plt.title('All Art Index Family (Monthly Average)')
+    # plt.show()
 
-# Art GBP (already inflation adjusted)
-art_df_GBP = get_art_data('data\Art\All Art Index Family\index_values.json')
-# plt.plot(art_df_GBP['Date'], art_df_GBP['Index Value'])
-# plt.xlabel('Date')
-# plt.ylabel('Index Value')
-# plt.title('All Art Index Family (Monthly Average)')
-# plt.show()
+    # Art needs to be converted to USD
+    art_df = convert_to_USD(art_df_GBP, USD2_rates)
+    # plt.plot(art_df['Date'], art_df['Index Value'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Index Value')
+    # plt.title('All Art Index Family (Monthly Average) USD')
+    # plt.show()
 
-# Art needs to be converted to USD
-art_df = convert_to_USD(art_df_GBP, USD2_rates)
-# plt.plot(art_df['Date'], art_df['Index Value'])
-# plt.xlabel('Date')
-# plt.ylabel('Index Value')
-# plt.title('All Art Index Family (Monthly Average) USD')
-# plt.show()
+    # CPI Index United States (USD) # Seasonally adjusted
+    cpi_df = get_CPI_Index(r'data\Correlated Variables\CPI Index\United States\CPI Data all items United States.csv')
+    # plt.plot(cpi_df['Date'], cpi_df['CPI_Index'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Index Value')
+    # plt.title('Monthly CPI_Index (Inflation) United States')
+    # plt.show()
 
-# CPI Index United States (USD) # Seasonally adjusted
-cpi_df = get_CPI_Index(r'data\Correlated Variables\CPI Index\United States\CPI Data all items United States.csv')
-# plt.plot(cpi_df['Date'], cpi_df['CPI_Index'])
-# plt.xlabel('Date')
-# plt.ylabel('Index Value')
-# plt.title('Monthly CPI_Index (Inflation) United States')
-# plt.show()
+    # Global Crypto Market Cap (USD)
+    crypto_df = get_monthly_global_crypto_market_cap(r'data\Correlated Variables\Crypto\global_marketcap.csv')
+    # plt.plot(crypto_df['Date'], crypto_df['Market cap (monthly)'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Market cap (USD)')
+    # plt.title('Global Crypto Marketcap (Monthly Average)')
+    # plt.show()
 
-# Global Crypto Market Cap (USD)
-crypto_df = get_monthly_global_crypto_market_cap(r'data\Correlated Variables\Crypto\global_marketcap.csv')
-# plt.plot(crypto_df['Date'], crypto_df['Market cap (monthly)'])
-# plt.xlabel('Date')
-# plt.ylabel('Market cap (USD)')
-# plt.title('Global Crypto Marketcap (Monthly Average)')
-# plt.show()
+    # Gold Prices (USD)
+    gold_df = get_monthly_gold_prices(r'data\Correlated Variables\Gold Prices\data.csv')
+    # plt.plot(gold_df['Date'], gold_df['Gold Price (monthly)'])
+    # plt.xlabel('Date')
+    # plt.ylabel('Gold Price (USD)')
+    # plt.title('Gold Prices (Monthly Average)')
+    # plt.show()
 
-# Gold Prices (USD)
-gold_df = get_monthly_gold_prices(r'data\Correlated Variables\Gold Prices\data.csv')
-# plt.plot(gold_df['Date'], gold_df['Gold Price (monthly)'])
-# plt.xlabel('Date')
-# plt.ylabel('Gold Price (USD)')
-# plt.title('Gold Prices (Monthly Average)')
-# plt.show()
+    # SP500 (USD) 
+    sp500_df = get_sp500(r'data\Correlated Variables\S&P 500\data.csv')
+    # plt.plot(sp500_df['Date'], sp500_df['Real'])
+    # plt.xlabel('Date')
+    # plt.ylabel('S&P 500 Index Value (REAL)')
+    # plt.title('S&P 500 Monthly (USD) Inflation Adjusted')
+    # plt.show()
+    # Now plot SP500 nominal (not inflation adjusted)
+    # plt.plot(sp500_df['Date'], sp500_df['Nominal'])
+    # plt.xlabel('Date')
+    # plt.ylabel('S&P 500 Index Value (NOMINAL)')
+    # plt.title('S&P 500 Monthly (USD) NOT Inflation Adjusted')
+    # plt.show()
 
-# SP500 (USD) 
-sp500_df = get_sp500(r'data\Correlated Variables\S&P 500\data.csv')
-# plt.plot(sp500_df['Date'], sp500_df['Real'])
-# plt.xlabel('Date')
-# plt.ylabel('S&P 500 Index Value (REAL)')
-# plt.title('S&P 500 Monthly (USD) Inflation Adjusted')
-# plt.show()
-# Now plot SP500 nominal (not inflation adjusted)
-# plt.plot(sp500_df['Date'], sp500_df['Nominal'])
-# plt.xlabel('Date')
-# plt.ylabel('S&P 500 Index Value (NOMINAL)')
-# plt.title('S&P 500 Monthly (USD) NOT Inflation Adjusted')
-# plt.show()
+    # United States 10-Year Bond Yield (USD) in %
+    bond_yield_df = get_US10_year_bond_yield(r'data\Correlated Variables\United States 10 Years Government Bond Yield\data_modified.csv')
+    # plt.plot(bond_yield_df['Date'], bond_yield_df['Rate_in_Percent'])
+    # plt.xlabel('Date')
+    # plt.ylabel('10 Year Bond Yield (%)')
+    # plt.title('United States 10-Year Bond Yield (Monthly) USD (%)')
+    # plt.show()
 
-# United States 10-Year Bond Yield (USD) in %
-bond_yield_df = get_US10_year_bond_yield(r'data\Correlated Variables\United States 10 Years Government Bond Yield\data_modified.csv')
-# plt.plot(bond_yield_df['Date'], bond_yield_df['Rate_in_Percent'])
-# plt.xlabel('Date')
-# plt.ylabel('10 Year Bond Yield (%)')
-# plt.title('United States 10-Year Bond Yield (Monthly) USD (%)')
-# plt.show()
+    ## Now adjust all the data for inflation ##
 
-## Now adjust all the data for inflation ##
+    # NB : Art index is already adjusted for inflation in its original data
+    yearly_cpi_df = get_cpi_yearly_rates(cpi_df)
+    wine_df = adjust_inflation(wine_df, yearly_cpi_df)
+    watch_df = adjust_inflation(watch_df, yearly_cpi_df)
 
-# NB : Art index is already adjusted for inflation in its original data
-yearly_cpi_df = get_cpi_yearly_rates(cpi_df)
-wine_df = adjust_inflation(wine_df, yearly_cpi_df)
-watch_df = adjust_inflation(watch_df, yearly_cpi_df)
+    # Adjust the correlated variable's inflation:
+    # CPI Index is inflation itself no need to adjust
+    # SP500 is already adjusted for inflation (Real column)
+    crypto_df = adjust_inflation(crypto_df, yearly_cpi_df)
+    gold_df = adjust_inflation(gold_df, yearly_cpi_df)
 
-# Adjust the correlated variable's inflation:
-# CPI Index is inflation itself no need to adjust
-# SP500 is already adjusted for inflation (Real column)
-crypto_df = adjust_inflation(crypto_df, yearly_cpi_df)
-gold_df = adjust_inflation(gold_df, yearly_cpi_df)
+    # Adjust inflation on the ten year bond yield
+    df_inflation_percent = calculate_inflation_percent_yearly(yearly_cpi_df)
+    bond_yield_df = adjust_bond_yield_inflation(bond_yield_df, df_inflation_percent)
 
-# Adjust inflation on the ten year bond yield
-df_inflation_percent = calculate_inflation_percent_yearly(yearly_cpi_df)
-bond_yield_df = adjust_bond_yield_inflation(bond_yield_df, df_inflation_percent)
+    ## Log transform the data ##
 
-## Log transform the data ##
+    # Asset Class Data
+    wine_df = log_transform(wine_df)
+    watch_df = log_transform(watch_df)
+    art_df = log_transform(art_df)
 
-# Asset Class Data
-wine_df = log_transform(wine_df)
-watch_df = log_transform(watch_df)
-art_df = log_transform(art_df)
+    # Correlated Variables
+    crypto_df = log_transform(crypto_df)
+    gold_df = log_transform(gold_df)
+    sp500_df = log_transform_sp500(sp500_df)
+    cpi_df = log_transform_cpi(cpi_df) 
+    # NB : Bond yield is already in percent (Ratio) so no need to log transform
 
-# Correlated Variables
-crypto_df = log_transform(crypto_df)
-gold_df = log_transform(gold_df)
-sp500_df = log_transform_sp500(sp500_df)
-cpi_df = log_transform_cpi(cpi_df) 
-# NB : Bond yield is already in percent (Ratio) so no need to log transform
-
-## Ready for analysis ##
+    ## Ready for analysis ##
+    return wine_df, watch_df, art_df, crypto_df, gold_df, sp500_df, cpi_df, bond_yield_df
 
  
 
