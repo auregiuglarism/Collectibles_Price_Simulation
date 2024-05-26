03273424e14df3753883bced71ce4e650063e84d
Aurelien Giuglaris-Michael <aurelien.giuglarism@gmail.com>
2024-05-26 19:46:39 +0200

    corrected multivariate bug
    now multivariate forecasting works for watch and wine

--- multivariate.py
+++ multivariate.py
@@ -167,7 +167,7 @@ def compute_t_test(index_df, variable, significance_level=0.05):
 
 def create_model(train, order, exogenous_var, seasonal_order=None, index='wine'):
     if seasonal_order == None: # ARIMAX Model
-        model = SARIMAX(train, trend='n', order=order,
+        model = ARIMA(train, trend='n', order=order,
                       exog=exogenous_var,  
                       enforce_stationarity=True,
                       enforce_invertibility=True) 
@@ -192,16 +192,16 @@ def create_model(train, order, exogenous_var, seasonal_order=None, index='wine')
 
     return fit_results, training_residuals
 
-def test_model(test, model=None, seasonal=False, index='wine'): # Testing data
+def test_model(test, test_exog, model=None, seasonal=False, index='wine'): # Testing data
     if model == None and seasonal == False: # ARIMA Model
         model = ARIMAResults.load(f'models\{index}_arimax.pkl')
     
     elif model == None and seasonal == True: # SARIMA Model
         model = ARIMAResults.load(f'models\{index}_sarimax.pkl')
 
-    # Testing Forecast
+    # Testing Forecast make sure test_exog has same length as test.shape[0]
     forecast_steps = test.shape[0]
-    forecast = model.get_forecast(steps=forecast_steps)
+    forecast = model.get_forecast(steps=forecast_steps, exog=test_exog)
     forecast_ci = forecast.conf_int()
     yhat_test = forecast.predicted_mean.values # Apply the exp transformation if you used log transform before to invert scales back
 
@@ -263,10 +263,12 @@ def split_cross_validation(data, order, exog, index='wine', seasonal_order=None,
         train = split_data[:int(0.8*len(split_data))]
         test = split_data[int(0.8*len(split_data)):]
 
-        print(exog)
-        
-        fit_results, _ = create_model(train, order, exog, seasonal_order, index)
-        _, mae, mse, mae_baseline, mse_baseline, mae_baseline_mean, mse_baseline_mean, rmse, rmse_baseline, rmse_baseline_mean, mape, mape_baseline, mape_baseline_mean = test_model(test, fit_results, seasonal, index)
+        split_exog = exog[:int(split*len(exog))]
+        exog_train = split_exog[:int(0.8*len(split_exog))]
+        exog_test = split_exog[int(0.8*len(split_exog)):]
+
+        fit_results, _ = create_model(train, order, exog_train, seasonal_order, index)
+        _, mae, mse, mae_baseline, mse_baseline, mae_baseline_mean, mse_baseline_mean, rmse, rmse_baseline, rmse_baseline_mean, mape, mape_baseline, mape_baseline_mean = test_model(test, exog_test, fit_results, seasonal, index)
 
         # Model Evaluation Metrics
         mae_l.append(mae)
@@ -296,7 +298,7 @@ def evaluate_model_with_Plots(data, candidates, eval_df, exog, seasonal=False, i
     for candidate in candidates:
         if seasonal == False:
             # Split cross validation
-            aic, bic, mae, mse, rmse, mape, mae_bas, mse_bas, rmse_bas, mape_bas, mae_mean, mse_mean, rmse_mean, mape_mean = split_cross_validation(data, candidate, index, None, seasonal)
+            aic, bic, mae, mse, rmse, mape, mae_bas, mse_bas, rmse_bas, mape_bas, mae_mean, mse_mean, rmse_mean, mape_mean = split_cross_validation(data, candidate, exog, index, None, seasonal)
             
             # Store evaluation information (those are already avg calculated in the split cross validation function)
             eval_df.loc[len(eval_df)] = [candidate, None, aic, bic, mae, mse, rmse, mape]
@@ -396,7 +398,7 @@ watch_pearson_coeff = compute_pearson_coeff(watch_pearson_coeff, np.log(watch_df
 art_pearson_coeff = pd.DataFrame(columns = ["Crypto", "Gold", "SP500", "CPI", "Bond Yield"])
 art_pearson_coeff = compute_pearson_coeff(art_pearson_coeff, np.log(art_df.observed), [np.log(crypto_df.observed), np.log(gold_df.observed), np.log(sp500_df.observed), np.log(cpi_df.observed), bond_yield_df.observed])
 # print(wine_pearson_coeff)
-# print(watch_pearson_coeff)
+print(watch_pearson_coeff)
 # print(art_pearson_coeff)
 
 # Big correlation between Gold and Wine, Crypto and Watch, SP500+CPI and Art
@@ -410,16 +412,27 @@ art_pearson_coeff = compute_pearson_coeff(art_pearson_coeff, np.log(art_df.obser
 
 # WINE
 arima_wine = (3,1,3)
-wine_adjusted, exog_wine = align_data(wine_df.observed, gold_df.observed)
-print(wine_adjusted)
-print(exog_wine)
+# wine_adjusted, exog_wine = align_data(wine_df.observed, gold_df.observed)
+
+# Evaluate the model
+# eval_df = pd.DataFrame(columns=["ARIMA", "SEASONAL", "AIC", "BIC", "MAE", "MSE", "RMSE", "MAPE %"]) # To store the most important evaluation metrics
+# eval_df = evaluate_model_with_Plots(wine_adjusted, [arima_wine], eval_df, exog_wine, seasonal=False, index='wine')
+# print(eval_df)
+
+# Save optimal model
+# wine_model_exog = create_model(wine_adjusted, arima_wine, exog_wine, index='wine') 
+
+# WATCH
+arima_watch = (2,1,3)
+watch_adjusted, exog_watch = align_data(watch_df.observed, cpi_df.observed)
+
 # Evaluate the model
 eval_df = pd.DataFrame(columns=["ARIMA", "SEASONAL", "AIC", "BIC", "MAE", "MSE", "RMSE", "MAPE %"]) # To store the most important evaluation metrics
-eval_df = evaluate_model_with_Plots(wine_adjusted, [arima_wine], eval_df, exog_wine, seasonal=False, index='wine')
+eval_df = evaluate_model_with_Plots(watch_adjusted, [arima_watch], eval_df, exog_watch, seasonal=False, index='watch')
 print(eval_df)
 
 # Save optimal model
-# wine_model_exog = create_model(wine_df.observed, arima_wine, exog_wine, index='wine') 
+# watch_model_exog = create_model(watch_adjusted, arima_watch, exog_watch, index='watch')
 
 
 
